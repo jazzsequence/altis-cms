b209971081cc64d7f780dccb490f5361be1242fc
Chris Reynolds <chris@jazzsequence.com>
2020-12-09 19:25:15 -0700

    refactor the load file
    we need a way to get the config, and we're defining the defaults for jazzsequence.com

--- load.php
+++ load.php
@@ -7,24 +7,20 @@
 
 namespace Altis\CMS; // phpcs:ignore
 
-use Altis;
-
-// Register core module.
-add_action( 'altis.modules.init', function () {
-	$default_settings = [
-		'enabled'       => true,
-		'branding'      => true,
-		'large-network' => true,
-		'login-logo'    => '/vendor/altis/cms/assets/logo.svg',
+function get_config() {
+	return [
+		'enabled' => true,
+		'branding' => true,
+		'large-network' => false,
+		'login-logo' => '/vendor/altis/cms/assets/logo.svg',
 		'shared-blocks' => true,
-		'default-theme' => 'base',
-		'remove-emoji'  => true,
-		'xmlrpc'        => true,
-		'feeds'         => true,
-		'cloner'        => true,
-		'network-ui'    => [
+		'default-theme' => 'twentytwentyone',
+		'remove-emoji' => true,
+		'xmlrpc' => false,
+		'feeds' => true,
+		'cloner' => false,
+		'network-ui' => [
 			'disable-spam' => true,
 		],
 	];
-	Altis\register_module( 'cms', __DIR__, 'CMS', $default_settings, __NAMESPACE__ . '\\bootstrap' );
-}, 5 );
+}
